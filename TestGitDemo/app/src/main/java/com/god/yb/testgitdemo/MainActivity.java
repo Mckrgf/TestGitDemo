0ad1de2df86170cd3d8bc4c34ca0029cfc4b6549
YBing1992 <1124507487@qq.com>
2017-11-29 15:36:04 +0800

    15b提交

--- TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/MainActivity.java
+++ TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/MainActivity.java
@@ -1,13 +1,48 @@
 package com.god.yb.testgitdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.zhy.adapter.recyclerview.CommonAdapter;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv_list)
+    RecyclerView rvList;
+    @BindView(R.id.activity_main)
+    LinearLayout activityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        List<String> mdata = new ArrayList<>();
+        mdata.add("1");
+        mdata.add("2");
+        mdata.add("3");
+        mdata.add("4");
+
+        //设置布局类型
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvList.setLayoutManager(manager);
+        rvList.setAdapter(new CommonAdapter(this,R.layout.item_rv_test,mdata) {
+            @Override
+            protected void convert(ViewHolder holder, Object o, int position) {
+                holder.setText(R.id.tv,"zuowei"+position);
+            }
+        });
     }
 }
