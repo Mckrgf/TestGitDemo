3ee45c6fd3aca024f7631de830d5376ab6e13105
yaobing1992 <1124507487@qq.com>
2018-03-09 00:25:13 +0800

    于杭州更新，新增双击退出，增加fragment模块1

--- TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/activities/FragmentActivity.java
+++ TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/activities/FragmentActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
+import com.god.yb.testgitdemo.Fragments.FragmentA;
 import com.god.yb.testgitdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 包含Fragment、菜单按钮
@@ -13,16 +18,24 @@ import com.god.yb.testgitdemo.R;
 
 public class FragmentActivity extends BaseActivity {
     private static final String TAG = "FragmentActivity";
+    @BindView(R.id.fl_container)
+    FrameLayout flContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_other);
+        setContentView(R.layout.activity_fragment);
+        initView();
+        ButterKnife.bind(this);
+    }
+
+    private void initView() {
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_container, FragmentA.newInstense("测试数据"),"fragmentA").commit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_a,menu);
+        getMenuInflater().inflate(R.menu.menu_a, menu);
         return true;
     }
 
@@ -30,10 +43,10 @@ public class FragmentActivity extends BaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add_item:
-                Log.i(TAG,"Add item");
+                Log.i(TAG, "Add item");
                 break;
             case R.id.delete_item:
-                Log.i(TAG,"Detele item");
+                Log.i(TAG, "Detele item");
                 break;
         }
         return true;
