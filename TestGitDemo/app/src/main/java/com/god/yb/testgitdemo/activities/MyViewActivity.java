6eda78490845c8430621e638298b15a1d575f900
yaobing1992 <1124507487@qq.com>
2018-06-07 08:40:15 +0800

    自定义键盘

--- TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/activities/MyViewActivity.java
+++ TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/activities/MyViewActivity.java
@@ -23,22 +23,17 @@ public class MyViewActivity extends BaseActivity {
     EditText etTest;
     @BindView(R.id.my_view)
     KeyboardView myView;
-    @BindView(R.id.hide)
-    Button hide;
-    private boolean aaa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_view);
-
         ButterKnife.bind(this);
-        final KeyUtils keyUtils = new KeyUtils(this,etTest);
+        final KeyUtils keyUtils = new KeyUtils(this, etTest);
         etTest.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-
-                ((Activity)getContext()).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+                ((Activity) getContext()).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
                 try {
                     Class<EditText> cls = EditText.class;
                     Method setSoftInputShownOnFocus;
@@ -48,28 +43,11 @@ public class MyViewActivity extends BaseActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-
-                aaa = hasFocus;
-                int type = etTest.getInputType();
-                if (type== 8194) {
-                    if (hasFocus) {
-                        keyUtils.show();
-                    } else {
-                        keyUtils.hide();
-                    }
-                }
-            }
-        });
-        hide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (aaa) {
+                if (hasFocus) {
                     keyUtils.show();
                 } else {
                     keyUtils.hide();
                 }
-                aaa = !aaa;
             }
         });
 
