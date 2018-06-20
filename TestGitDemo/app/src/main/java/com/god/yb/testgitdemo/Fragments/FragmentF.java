df8db5d337e91634440154c93316dd6aacedd2fb
yaobing1992 <1124507487@qq.com>
2018-06-20 15:59:28 +0800

    自定义搜索框

--- TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/Fragments/FragmentF.java
+++ TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/Fragments/FragmentF.java
@@ -8,11 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.god.yb.testgitdemo.R;
 import com.god.yb.testgitdemo.viewSearch.adapter.SearchAdapter;
@@ -32,71 +29,17 @@ public class FragmentF extends Fragment implements SearchView.SearchViewListener
     private Activity mActivity;
     private static String Fragment_key = "test_key";
 
-
-    /**
-     * 搜索结果列表view
-     */
-    private ListView lvResults;
-
-    /**
-     * 搜索view
-     */
     private SearchView searchView;
 
-
-    /**
-     * 热搜框列表adapter
-     */
-    private ArrayAdapter<String> hintAdapter;
-
-    /**
-     * 自动补全列表adapter
-     */
     private ArrayAdapter<String> autoCompleteAdapter;
 
-    /**
-     * 搜索结果列表adapter
-     */
     private SearchAdapter resultAdapter;
 
-    /**
-     * 数据库数据，总数据
-     */
     private List<Bean> dbData;
 
-    /**
-     * 热搜版数据
-     */
-    private List<String> hintData;
-
-    /**
-     * 搜索过程中自动补全数据
-     */
     private List<String> autoCompleteData;
 
-    /**
-     * 搜索结果的数据
-     */
     private List<Bean> resultData;
-
-    /**
-     * 默认提示框显示项的个数
-     */
-    private static int DEFAULT_HINT_SIZE = 4;
-
-    /**
-     * 提示框显示项的个数
-     */
-    private static int hintSize = DEFAULT_HINT_SIZE;
-
-//    /**
-//     * 设置提示框显示项的个数
-//     *
-//     * @param hintSize 提示框显示个数
-//     */
-//    public static void setHintSize(int hintSize) {
-//        this.hintSize = hintSize;
-//    }
 
     @Override
     public void onAttach(Context context) {
@@ -122,20 +65,11 @@ public class FragmentF extends Fragment implements SearchView.SearchViewListener
      * 初始化视图
      */
     private void initViews(View view) {
-//        lvResults = (ListView) view.findViewById(R.id.main_lv_search_results);
-        searchView = (SearchView) view.findViewById(R.id.main_search_layout);
+        searchView = view.findViewById(R.id.main_search_layout);
         //设置监听
         searchView.setSearchViewListener(this);
         //设置adapter
-        searchView.setTipsHintAdapter(hintAdapter);
         searchView.setAutoCompleteAdapter(autoCompleteAdapter);
-
-//        lvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-//                Toast.makeText(getContext(), position + "", Toast.LENGTH_SHORT).show();
-//            }
-//        });
     }
 
     /**
@@ -144,8 +78,6 @@ public class FragmentF extends Fragment implements SearchView.SearchViewListener
     private void initData() {
         //从数据库获取数据
         getDbData();
-        //初始化热搜版数据
-        getHintData();
         //初始化自动补全数据
         getAutoCompleteData(null);
         //初始化搜索结果数据
@@ -164,16 +96,6 @@ public class FragmentF extends Fragment implements SearchView.SearchViewListener
         }
     }
 
-    /**
-     * 获取热搜版data 和adapter
-     */
-    private void getHintData() {
-        hintData = new ArrayList<>(hintSize);
-        for (int i = 1; i <= hintSize; i++) {
-            hintData.add("热搜版" + i + "：Android自定义View");
-        }
-        hintAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, hintData);
-    }
 
     /**
      * 获取自动补全data 和adapter
@@ -181,15 +103,13 @@ public class FragmentF extends Fragment implements SearchView.SearchViewListener
     private void getAutoCompleteData(String text) {
         if (autoCompleteData == null) {
             //初始化
-            autoCompleteData = new ArrayList<>(hintSize);
+            autoCompleteData = new ArrayList<>();
         } else {
             // 根据text 获取auto data
             autoCompleteData.clear();
-            for (int i = 0, count = 0; i < dbData.size()
-                    && count < hintSize; i++) {
+            for (int i = 0; i < dbData.size(); i++) {
                 if (dbData.get(i).getTitle().contains(text.trim())) {
                     autoCompleteData.add(dbData.get(i).getTitle());
-                    count++;
                 }
             }
         }
@@ -235,21 +155,5 @@ public class FragmentF extends Fragment implements SearchView.SearchViewListener
     public void onRefreshAutoComplete(String text) {
         //更新数据
         getAutoCompleteData(text);
-    }
-
-    @Override
-    public void onSearch(String text) {
-        //更新result数据
-        getResultData(text);
-        lvResults.setVisibility(View.VISIBLE);
-        //第一次获取结果 还未配置适配器
-        if (lvResults.getAdapter() == null) {
-            //获取搜索数据 设置适配器
-            lvResults.setAdapter(resultAdapter);
-        } else {
-            //更新搜索数据
-            resultAdapter.notifyDataSetChanged();
-        }
-        Toast.makeText(getContext(), "完成搜素", Toast.LENGTH_SHORT).show();
     }
 }
