bead05fd83ca85a6b3ab88145d958f26cc4f4adb
Mckrgf <1124507487@qq.com>
2018-08-05 15:13:37 +0800

    update from macOS

--- TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/activities/HomeActivity.java
+++ TestGitDemo/app/src/main/java/com/god/yb/testgitdemo/activities/HomeActivity.java
@@ -105,6 +105,7 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
         daoSession = ((App) getApplication()).getDaoSession();
         userDao = daoSession.getUserDao();
